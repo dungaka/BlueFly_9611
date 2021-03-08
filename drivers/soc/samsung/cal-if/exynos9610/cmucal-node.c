whatawurst/android_kernel_samsung_exynos9820

drivers: soc: samsung: Fix divide by zero issues in macros
Signed-Off-by: Andreas Schneider <asn@cryptomilk.org>
Change-Id: Ie8c8e3bbd80b6d382fafce94c5962a20b2e4d0cc

drivers/soc/samsung/cal-if/exynos9820/cmucal-node.c
@@ -1078,78 +1078,78 @@ struct cmucal_mux cmucal_mux_list[] = {
 	CLK_MUX(CLKCMU_MIF_DDRPHY2X_S2D, cmucal_clkcmu_mif_ddrphy2x_s2d_parents, CLK_CON_MUX_CLKCMU_MIF_DDRPHY2X_S2D_SELECT, CLK_CON_MUX_CLKCMU_MIF_DDRPHY2X_S2D_BUSY, CLK_CON_MUX_CLKCMU_MIF_DDRPHY2X_S2D_ENABLE_AUTOMATIC_CLKGATING),
 	CLK_MUX(MUX_CLK_S2D_CORE, cmucal_mux_clk_s2d_core_parents, CLK_CON_MUX_MUX_CLK_S2D_CORE_SELECT, CLK_CON_MUX_MUX_CLK_S2D_CORE_BUSY, CLK_CON_MUX_MUX_CLK_S2D_CORE_ENABLE_AUTOMATIC_CLKGATING),
 	CLK_MUX(MUX_CLK_VTS_BUS, cmucal_mux_clk_vts_bus_parents, CLK_CON_MUX_MUX_CLK_VTS_BUS_SELECT, CLK_CON_MUX_MUX_CLK_VTS_BUS_BUSY, CLK_CON_MUX_MUX_CLK_VTS_BUS_ENABLE_AUTOMATIC_CLKGATING),
-	CLK_MUX(APM_CMU_APM_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(APM_CMU_APM_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(AUD_CMU_AUD_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(AUD_CMU_AUD_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(BUSC_CMU_BUSC_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(BUSC_CMU_BUSC_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(CMGP_CMU_CMGP_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(CMGP_CMU_CMGP_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(CMU_CMU_CMU_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(CMU_CMU_CMU_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(CORE_CMU_CORE_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(CORE_CMU_CORE_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(CPUCL0_CMU_CPUCL0_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(CPUCL0_CMU_CPUCL0_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(CPUCL0_EMBEDDED_CMU_CPUCL0_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(CPUCL0_EMBEDDED_CMU_CPUCL0_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(CPUCL1_CMU_CPUCL1_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(CPUCL1_CMU_CPUCL1_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(CPUCL2_CMU_CPUCL2_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(CPUCL2_CMU_CPUCL2_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(CPUCL2_EMBEDDED_CMU_CPUCL2_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(CPUCL2_EMBEDDED_CMU_CPUCL2_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(DPU_CMU_DPU_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(DPU_CMU_DPU_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(DSPM_CMU_DSPM_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(DSPM_CMU_DSPM_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(DSPS_CMU_DSPS_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(DSPS_CMU_DSPS_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(FSYS0_CMU_FSYS0_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(FSYS0_CMU_FSYS0_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(FSYS0A_CMU_FSYS0A_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(FSYS0A_CMU_FSYS0A_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(FSYS1_CMU_FSYS1_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(FSYS1_CMU_FSYS1_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(G2D_CMU_G2D_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(G2D_CMU_G2D_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(G3D_CMU_G3D_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(G3D_CMU_G3D_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(G3D_EMBEDDED_CMU_G3D_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(G3D_EMBEDDED_CMU_G3D_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(ISPHQ_CMU_ISPHQ_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(ISPHQ_CMU_ISPHQ_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(ISPLP_CMU_ISPLP_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(ISPLP_CMU_ISPLP_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(ISPPRE_CMU_ISPPRE_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(ISPPRE_CMU_ISPPRE_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(IVA_CMU_IVA_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(IVA_CMU_IVA_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(MFC_CMU_MFC_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(MFC_CMU_MFC_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(MIF_CMU_MIF_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(MIF_CMU_MIF_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(MIF1_CMU_MIF1_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(MIF1_CMU_MIF1_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(MIF2_CMU_MIF2_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(MIF2_CMU_MIF2_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(MIF3_CMU_MIF3_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(MIF3_CMU_MIF3_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(NPU0_CMU_NPU0_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(NPU0_CMU_NPU0_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(NPU1_CMU_NPU1_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(NPU1_CMU_NPU1_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(PERIC0_CMU_PERIC0_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(PERIC0_CMU_PERIC0_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(PERIC1_CMU_PERIC1_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(PERIC1_CMU_PERIC1_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(PERIS_CMU_PERIS_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(PERIS_CMU_PERIS_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(VRA2_CMU_VRA2_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(VRA2_CMU_VRA2_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(VTS_CMU_VTS_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(VTS_CMU_VTS_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(APM_CMU_APM_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(APM_CMU_APM_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(AUD_CMU_AUD_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(AUD_CMU_AUD_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(BUSC_CMU_BUSC_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(BUSC_CMU_BUSC_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(CMGP_CMU_CMGP_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(CMGP_CMU_CMGP_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(CMU_CMU_CMU_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(CMU_CMU_CMU_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(CORE_CMU_CORE_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(CORE_CMU_CORE_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(CPUCL0_CMU_CPUCL0_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(CPUCL0_CMU_CPUCL0_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(CPUCL0_EMBEDDED_CMU_CPUCL0_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(CPUCL0_EMBEDDED_CMU_CPUCL0_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(CPUCL1_CMU_CPUCL1_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(CPUCL1_CMU_CPUCL1_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(CPUCL2_CMU_CPUCL2_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(CPUCL2_CMU_CPUCL2_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(CPUCL2_EMBEDDED_CMU_CPUCL2_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(CPUCL2_EMBEDDED_CMU_CPUCL2_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(DPU_CMU_DPU_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(DPU_CMU_DPU_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(DSPM_CMU_DSPM_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(DSPM_CMU_DSPM_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(DSPS_CMU_DSPS_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(DSPS_CMU_DSPS_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(FSYS0_CMU_FSYS0_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(FSYS0_CMU_FSYS0_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(FSYS0A_CMU_FSYS0A_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(FSYS0A_CMU_FSYS0A_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(FSYS1_CMU_FSYS1_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(FSYS1_CMU_FSYS1_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(G2D_CMU_G2D_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(G2D_CMU_G2D_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(G3D_CMU_G3D_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(G3D_CMU_G3D_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(G3D_EMBEDDED_CMU_G3D_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(G3D_EMBEDDED_CMU_G3D_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(ISPHQ_CMU_ISPHQ_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(ISPHQ_CMU_ISPHQ_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(ISPLP_CMU_ISPLP_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(ISPLP_CMU_ISPLP_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(ISPPRE_CMU_ISPPRE_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(ISPPRE_CMU_ISPPRE_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(IVA_CMU_IVA_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(IVA_CMU_IVA_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(MFC_CMU_MFC_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(MFC_CMU_MFC_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(MIF_CMU_MIF_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(MIF_CMU_MIF_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(MIF1_CMU_MIF1_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(MIF1_CMU_MIF1_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(MIF2_CMU_MIF2_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(MIF2_CMU_MIF2_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(MIF3_CMU_MIF3_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(MIF3_CMU_MIF3_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(NPU0_CMU_NPU0_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(NPU0_CMU_NPU0_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(NPU1_CMU_NPU1_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(NPU1_CMU_NPU1_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(PERIC0_CMU_PERIC0_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(PERIC0_CMU_PERIC0_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(PERIC1_CMU_PERIC1_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(PERIC1_CMU_PERIC1_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(PERIS_CMU_PERIS_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(PERIS_CMU_PERIS_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(VRA2_CMU_VRA2_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(VRA2_CMU_VRA2_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(VTS_CMU_VTS_CLKOUT0, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(VTS_CMU_VTS_CLKOUT1, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
 	CLK_MUX(MUX_CLKCMU_APM_BUS_USER, cmucal_mux_clkcmu_apm_bus_user_parents, PLL_CON0_MUX_CLKCMU_APM_BUS_USER_MUX_SEL, PLL_CON0_MUX_CLKCMU_APM_BUS_USER_BUSY, PLL_CON2_MUX_CLKCMU_APM_BUS_USER_ENABLE_AUTOMATIC_CLKGATING),
 	CLK_MUX(MUX_DLL_USER, cmucal_mux_dll_user_parents, PLL_CON0_MUX_DLL_USER_MUX_SEL, PLL_CON0_MUX_DLL_USER_BUSY, PLL_CON2_MUX_DLL_USER_ENABLE_AUTOMATIC_CLKGATING),
 	CLK_MUX(MUX_CLKMUX_APM_RCO_USER, cmucal_mux_clkmux_apm_rco_user_parents, PLL_CON0_MUX_CLKMUX_APM_RCO_USER_MUX_SEL, PLL_CON0_MUX_CLKMUX_APM_RCO_USER_BUSY, PLL_CON2_MUX_CLKMUX_APM_RCO_USER_ENABLE_AUTOMATIC_CLKGATING),

dungaka/BlueFly_9611

drivers/soc/samsung/cal-if/exynos9610/cmucal-node.c
@@ -634,28 +634,28 @@ struct cmucal_mux cmucal_mux_list[] = {
 	CLK_MUX(MUX_CLK_SHUB_USI00, cmucal_mux_clk_shub_usi00_parents, CLK_CON_MUX_MUX_CLK_SHUB_USI00_SELECT, CLK_CON_MUX_MUX_CLK_SHUB_USI00_BUSY, CLK_CON_MUX_MUX_CLK_SHUB_USI00_ENABLE_AUTOMATIC_CLKGATING),
 	CLK_MUX(MUX_CLK_SHUB_USI01, cmucal_mux_clk_shub_usi01_parents, CLK_CON_MUX_MUX_CLK_SHUB_USI01_SELECT, CLK_CON_MUX_MUX_CLK_SHUB_USI01_BUSY, CLK_CON_MUX_MUX_CLK_SHUB_USI01_ENABLE_AUTOMATIC_CLKGATING),
 	CLK_MUX(MUX_CLK_SHUB_I2C, cmucal_mux_clk_shub_i2c_parents, CLK_CON_MUX_MUX_CLK_SHUB_I2C_SELECT, CLK_CON_MUX_MUX_CLK_SHUB_I2C_BUSY, CLK_CON_MUX_MUX_CLK_SHUB_I2C_ENABLE_AUTOMATIC_CLKGATING),
-	CLK_MUX(APM_CMU_APM_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(CAM_CMU_CAM_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(CMGP_CMU_CMGP_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(CMU_CMU_TOP_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(CORE_CMU_CORE_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(CPUCL0_CMU_CPUCL0_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(CPUCL0_EMBEDDED_CMU_CPUCL0_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(CPUCL1_CMU_CPUCL1_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(CPUCL1_EMBEDDED_CMU_CPUCL1_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(DISPAUD_CMU_DISPAUD_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(FSYS_CMU_FSYS_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(G2D_CMU_G2D_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(G3D_CMU_G3D_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(ISP_CMU_ISP_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(MFC_CMU_MFC_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(MIF_CMU_MIF_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(MIF1_CMU_MIF1_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(PERI_CMU_PERI_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(SHUB_CMU_SHUB_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(USB_CMU_USB_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(VIPX1_CMU_VIPX1_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
-	CLK_MUX(VIPX2_CMU_VIPX2_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(APM_CMU_APM_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(CAM_CMU_CAM_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(CMGP_CMU_CMGP_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(CMU_CMU_TOP_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(CORE_CMU_CORE_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(CPUCL0_CMU_CPUCL0_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(CPUCL0_EMBEDDED_CMU_CPUCL0_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(CPUCL1_CMU_CPUCL1_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(CPUCL1_EMBEDDED_CMU_CPUCL1_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(DISPAUD_CMU_DISPAUD_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(FSYS_CMU_FSYS_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(G2D_CMU_G2D_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(G3D_CMU_G3D_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(ISP_CMU_ISP_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(MFC_CMU_MFC_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(MIF_CMU_MIF_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(MIF1_CMU_MIF1_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(PERI_CMU_PERI_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(SHUB_CMU_SHUB_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(USB_CMU_USB_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(VIPX1_CMU_VIPX1_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
+	CLK_MUX2(VIPX2_CMU_VIPX2_CLKOUT, NULL, EMPTY_CAL_ID, EMPTY_CAL_ID, EMPTY_CAL_ID),
 	CLK_MUX(MUX_CLKCMU_APM_BUS_USER, cmucal_mux_clkcmu_apm_bus_user_parents, PLL_CON0_MUX_CLKCMU_APM_BUS_USER_MUX_SEL, PLL_CON0_MUX_CLKCMU_APM_BUS_USER_BUSY, PLL_CON2_MUX_CLKCMU_APM_BUS_USER_ENABLE_AUTOMATIC_CLKGATING),
 	CLK_MUX(MUX_DLL_USER, cmucal_mux_dll_user_parents, PLL_CON0_MUX_DLL_USER_MUX_SEL, PLL_CON0_MUX_DLL_USER_BUSY, PLL_CON2_MUX_DLL_USER_ENABLE_AUTOMATIC_CLKGATING),
 	CLK_MUX(MUX_CLKCMU_CAM_BUS_USER, cmucal_mux_clkcmu_cam_bus_user_parents, PLL_CON0_MUX_CLKCMU_CAM_BUS_USER_MUX_SEL, PLL_CON0_MUX_CLKCMU_CAM_BUS_USER_BUSY, PLL_CON2_MUX_CLKCMU_CAM_BUS_USER_ENABLE_AUTOMATIC_CLKGATING),